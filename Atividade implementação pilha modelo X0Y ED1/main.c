4e837d436d6600726d3e9c1dd2b15424a77e9ce5
Andrei <112513275+Andreirl032@users.noreply.github.com>
2023-05-14 01:27:06 -0300

    Add files via upload

---
+++ Atividade implementação pilha modelo X0Y ED1/main.c
@@ -0,0 +1,10 @@
+#include "Stack.h"
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+  char *oi = "AMEOPOVONATIMEYEMITANOVOPOEMA";
+  int len = strlen(oi);
+  EX0Y(oi, len);
+  return 0;
+}
